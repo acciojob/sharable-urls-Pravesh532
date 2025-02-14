b39a5e80ec82f3336d938959bf66c49dfcf2d34d
Pravesh532 <praveshpatel9569@gmail.com>
2025-02-14 23:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+ document.getElementById("myform").addEventListener("submit",function(event) {
+ 	event.preventDefault(); 
+
+	 let value = ducument.getElementById("name").value;
+	 let vlue2 = document.getElementById("year").value;
+ let updatedUrl = `https://localhost:8080/?name=${encodeURIComponent(nameValue)}&year=${encodeURIComponent(yearValue)}`; })
+ document.getElementById("url").textContent = updatedUrl;
